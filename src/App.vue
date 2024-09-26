1af1654b58ff2e15e3fd7b059beec2d0ff6d95bc
zano <zano@live.cl>
2024-09-26 20:42:46 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-
+import Balance from './components/Balance.vue';
+import Header from './components/Header.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
 </script>
 
 <template>
-  <h1>
-    Hello
-  </h1>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Balance.vue
@@ -0,0 +1,4 @@
+<template>
+    <h2>Your Balance</h2>
+    <h3 id="balance">$0.00</h3>
+</template>
