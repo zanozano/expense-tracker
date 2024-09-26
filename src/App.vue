56b1cffcfe71dac21b020938fc9d3ae9f21b3b56
zano <zano@live.cl>
2024-09-26 20:51:31 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import AddTransaction from './components/AddTransaction.vue';
 import Balance from './components/Balance.vue';
 import Header from './components/Header.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
@@ -11,6 +12,7 @@ import TransactionList from './components/TransactionList.vue';
     <Balance />
     <IncomeExpenses />
     <TransactionList />
+    <AddTransaction />
   </div>
 </template>
 
